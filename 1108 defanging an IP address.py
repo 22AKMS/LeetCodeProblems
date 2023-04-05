878a7b99dee08ce454f362fcc684b98f15338fc6
22AKMS <48903419+22AKMS@users.noreply.github.com>
2023-04-05 09:33:51 -0400

    forgor 💀

--- 1108 defanging an IP address.py
+++ 1108 defanging an IP address.py
@@ -1,3 +1,8 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return address.replace('.', '[.]')
+
+test=Solution()
+
+print(test.defangIPaddr("1.1.1.1"))
+print(test.defangIPaddr("255.100.50.0"))
